fcc3573562f5e7079c02c2b3f613d31153a38171
hacodeml <hacodeml@gmail.com>
2020-01-18 06:47:36 -0500

    feat: set up webpack with babel loaders to transpile JSX to JS

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h1>Setting up webpack with babel loaders to transpile JSX to JS</h1>,
+  document.getElementById("app")
+);
